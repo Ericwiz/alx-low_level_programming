4ca6c53b482ae7b57f7571ff13d765a50fe05dc1
EricWiz <wisdomagu44@gmail.com>
2022-09-08 14:54:53 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /**
 * main - Entry point
-* Description: Should print "\" Programming is like building a multilingual puzzle"
+* Description: prints "\"Programming is like building a multilingual puzzl"
 * Return: 0
-int main(void) {
+*/
+
+int main(void){
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
