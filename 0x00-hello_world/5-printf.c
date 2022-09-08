e09d643fee7c3b773d9c6a42adad79b2bba1ba1c
EricWiz <wisdomagu44@gmail.com>
2022-09-08 15:03:31 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - print a string
+* Description: use printf
+* Return: 0
+*/
+
+int main(void)
+{
+printf("%s", "with proper grammer, but the outcome is a piece of art, \n");
+return (0);
+}
