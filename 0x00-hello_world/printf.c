2c7c81072157c74fee5a4e6b9ebe347b6eeef2f4
EricWiz <wisdomagu44@gmail.com>
2022-09-08 15:10:52 -0700

    using printf

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: use printf
+* Return: 0
+*/
+int main(voi)
+{
+printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
